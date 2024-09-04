49863d155a1b3f926817be1fa5b5973471bfada0
Seongdeok Hong <sdhong0609@gmail.com>
2024-09-04 18:42:56 +0900

    feat: MainActivity에 Fragment로 TabLayout 적용

---
+++ app/src/main/java/com/hongstudio/marvelcharacters/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.hongstudio.marvelcharacters.ui
+
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.hongstudio.marvelcharacters.R
+import com.hongstudio.marvelcharacters.base.BaseActivity
+import com.hongstudio.marvelcharacters.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    inflater = ActivityMainBinding::inflate
+) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setUpView()
+    }
+
+    private fun setUpView() {
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.page_search_title)
+                1 -> getString(R.string.page_favorite_title)
+                else -> ""
+            }
+        }.attach()
+    }
+}
